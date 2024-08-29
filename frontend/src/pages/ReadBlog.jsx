15a0365bbf0d578a84ad9d229b8495d1e9f40154
theothedancecoder <165029858+theothedancecoder@users.noreply.github.com>
2024-08-29 16:20:34 +0200

    added a back button to return to home page on individual posts

--- frontend/src/pages/ReadBlog.jsx
+++ frontend/src/pages/ReadBlog.jsx
@@ -1,10 +1,11 @@
 import { getPost } from "../api"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 export function ReadBlog(){
     const [post, setPost]= useState({})
     let params = useParams()
+    const navigate = useNavigate()
     let id = params.id
 
      useEffect(()=>{
@@ -20,6 +21,7 @@ export function ReadBlog(){
 
     return(
         <> 
+        <button onClick={()=>navigate(-1)}>Back</button>
         <h1>{post.title}</h1>
         <h2>{post.description}</h2>
         <h3>{post.dateCreated}</h3>
