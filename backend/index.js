ab7132c867147f4b71e55d54b316abcc2630e052
Mian Khubaib AiF <miankhubaib63@gmail.com>
2024-09-12 22:17:11 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,44 @@
-const express = require('express')
+const connect = require("./connect")
+const express = require("express")
+const cors = require("cors")
+const posts = require("./postRoutes")
+const users = require ("./userRoutes")
+const awsRoutes = require("./awsRoutes")
+const multer =require("multer")
+const upload = multer()
 
-const app = express()
-const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
+
+
+const app =express()
+const PORT = 3000
+
+
+
+
+// ------
+
+
+
+
+app.use(express.json())
+app.use(upload.any()) // multer must come before route handlers else it will return undefined.
+app.use(posts)
+app.use(users)
+app.use(awsRoutes)
+app.listen(PORT, ()=>{
+    connect.connectToServer()
+    console.log(`server is running on port ${PORT}`)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+// Dummy route for testing
+app.get('/test', (req, res) => {
+    res.json({ message: "Backend is working fine!" });
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+
+
+
 
 // Export the Express API
 module.exports = app
